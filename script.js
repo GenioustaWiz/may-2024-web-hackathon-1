cb57e55f84c40c89a144e34c035b79d92c39b6eb
GenioustaWiz <cm0215557@gmail.com>
2024-07-01 11:52:27 +0300

    Portfolio project

---
+++ script.js
@@ -0,0 +1,10 @@
+const navLinks = document.querySelectorAll('nav a');
+
+navLinks.forEach(link => {
+  link.addEventListener('click', function(e) {
+    e.preventDefault();
+    const sectionId = this.getAttribute('href');
+    const section = document.querySelector(sectionId);
+    section.scrollIntoView({ behavior: 'smooth' });
+  });
+});
